51505474a5fee6508d6141ce355ecd86d044595a
Qiev <73715844+Qiev@users.noreply.github.com>
2021-11-14 16:30:27 +0500

    Add files via upload

---
+++ pr-32_prakt15_1_OAP_Davydova/one/one/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n= ");
+            int a = int.Parse(Console.ReadLine());
+            Stack stack = new Stack();
+            for (int i = 1; i <= a; i++)
+                stack.Push(i);
+            Console.WriteLine("Размерность стека " + stack.Count);
+
+            Console.WriteLine("Верхний элемент стека = " + stack.Peek());
+            Console.WriteLine("Размерность стека " + stack.Count);
+
+            Console.Write("Содержимое стека = ");
+            while (stack.Count != 0)
+                Console.Write("{0} ", stack.Pop());
+            Console.WriteLine("\nНовая размерность стека " + stack.Count);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ pr-32_prakt15_1_OAP_Davydova/two/two/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("t.txt",true);
+            string str = sr.ReadToEnd();
+            sr.Close();
+            Stack stack = new Stack();
+            bool flag = true;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '(') stack.Push(i);
+                else if (str[i] == ')')
+                {
+                    if (stack.Count == 0)
+                    { flag = false; Console.WriteLine("Возможно в позиции " + i + " лишняя скобка"); }
+                    else stack.Pop();
+                }
+            }
+            if (stack.Count == 0) { if (flag) Console.WriteLine("скобки сбалансированы"); }
+            {
+                Console.Write("Возможно лишняя ( скобка в позиции:");
+                while (stack.Count != 0)
+                {
+                    Console.Write("{0} ", (int)stack.Pop());
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
